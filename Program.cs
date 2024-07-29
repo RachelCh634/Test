8168720ad7a109a4c7dad984d5d43d76c52ee0fc
rachel634 <had4059@gmail.com>
2024-07-29 22:02:41 +0300

    hello Git

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Rachel");
+            Console.WriteLine("Hello Git");
         }
     }
 }
